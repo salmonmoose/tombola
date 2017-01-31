1d7445e61f6305821f4606029cb787610e8071f8
Amelia Rey Fletcher <salmonmoose@gmail.com>
2017-01-31 13:49:49 +1030

    fixes

--- controllers/default.py
+++ controllers/default.py
@@ -10,15 +10,73 @@
 
 
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
+    request.empty_page = True
 
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+    form = SQLFORM.factory(
+        Field('hash1', 'string',
+            length=4,
+            requires=IS_LENGTH(minsize=4, maxsize=4),
+            ),
+        Field('hash2', 'string',
+            length=4,
+            requires=IS_LENGTH(minsize=4, maxsize=4),
+            ),
+        Field('hash3', 'string',
+            length=4,
+            requires=IS_LENGTH(minsize=4, maxsize=4),
+            ),
+        Field('hash4', 'string',
+            length=4,
+            requires=IS_LENGTH(minsize=4, maxsize=4),
+            ),
+        Field('email', 'string',
+            requires=IS_EMAIL()
+            ),
+        Field('confirm', 'string',
+            rname='confirm_email',
+            requires=IS_EXPR(
+                'value==%s' % repr(request.vars.get('email', None)),
+                error_message="Emails do not match",
+                )
+            ),
+    )
+
+    if form.process().accepted:
+        request_key = '%s%s%s%s' % (
+            request.vars.hash1,
+            request.vars.hash2,
+            request.vars.hash3,
+            request.vars.hash4
+            )
+
+        secret_key = db(
+            (db.secret_key.secret_key == request_key)
+            ).select().first()
+
+        if secret_key is None:
+            response.flash = T("unknown key")
+
+        #TODO: requests from a single IP should be rate limited.
+
+        elif secret_key['redeemed']:
+            response.flash =T("key has already been redeemed")
+        else:
+            secret_key.update_record(
+                redeemed=True
+                )
+
+            db.entry.insert(
+                secret_key = secret_key.id,
+                email = request.vars.email,
+                ip_address = request.vars.client,
+                browser_hash = hashlib.sha1(request.env.http_user_agent).hexdigest()
+                )
+
+            response.flash = T("good luck")
+    else:
+        print "not accepted"
+
+    return dict(form=form)
 
 
 def user():
